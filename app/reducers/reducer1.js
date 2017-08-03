5fbf5cf91eabba70b2f36c59dab5ed665292a545
Aayush Anand <aayush.anand@metricstream.com>
2017-08-03 15:13:35 +0530

    redux promise axios and react-navigation added

--- app/reducers/reducer1.js
+++ app/reducers/reducer1.js
@@ -1,6 +1,6 @@
 export default function(state,action){
-	switch(action.type){
-		case "State responsible":return something//always return a fresh object
-	}
+	// switch(action.type){
+	// 	case "State responsible":return something//always return a fresh object
+	// }
 	return(state)//when the action is not related to the state taken care of
 }
